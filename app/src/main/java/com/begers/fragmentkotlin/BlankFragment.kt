75cebb6d25fa088a390a76d645b35c58a9a337d8
bekirgeris <bekirgeris@marun.edu.tr>
2021-12-07 09:59:12 +0300

    +firtsFragment go to secondFragment +secondFragment go to firstFragment

--- app/src/main/java/com/begers/fragmentkotlin/BlankFragment.kt
+++ app/src/main/java/com/begers/fragmentkotlin/BlankFragment.kt
@@ -12,10 +12,7 @@ class BlankFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blank, container, false)
     }
